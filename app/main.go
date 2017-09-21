4c1b85a0a7a02cdba6da6b09cd293f525be39030
aragonhk <michael.c.ng@live.com>
2017-09-21 10:05:15 -0700

    commit

--- app/main.go
+++ app/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
   "github.com/gin-gonic/gin"
-  "github.com/aragonhk/pulsecheck-back-go/app/controllers"
+  "github.com/aragonhk/pulsecheck-back-go/controllers"
   )
 //var router *gin.Engine
 
