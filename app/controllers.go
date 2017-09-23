9aa3d3eab49937ce99081c11f3d207b20ea5fcab
aragonhk <michael.c.ng@live.com>
2017-09-22 23:11:18 -0700

    added cors

--- app/controllers.go
+++ app/controllers.go
@@ -3,11 +3,14 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"github.com/gin-gonic/gin"
+  "github.com/gin-gonic/gin"
+  "github.com/gin-contrib/cors"
 )
 
 func initRoutes() *gin.Engine {
   r := gin.Default()
+  r.Use(cors.Default())
+
   r.GET("/", index)
 
   r.GET("/login", Login)
